15e91084192929fec24a04985b1edd65b02806a3
KartikHalkunde <kartikhalkunde26@gmail.com>
2026-09-18 12:54:36 +0530

    Added 1749. Maximum Absolute Sum of Any Subarray problem to the randoms

---
+++ randoms/1749. Maximum Absolute Sum of Any Subarray/maxAbsoluteSum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int max = nums[0];
+        int min = nums[0];
+        int result = Math.abs(nums[0]);
+
+        for(int i = 1; i < nums.length ; i++){
+            int curr = nums[i];
+             max = Math.max(curr, curr +  max);
+             min = Math.min(curr, curr +  min);
+            result = Math.max(result, Math.max(Math.abs(max),Math.abs(min)));
+        }
+        return result;
+    }
+}
