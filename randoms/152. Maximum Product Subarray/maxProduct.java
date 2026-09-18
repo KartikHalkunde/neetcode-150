7ec04aae9599f45ebb4d5fb3e58bda7515d1ec24
KartikHalkunde <kartikhalkunde26@gmail.com>
2026-09-18 11:34:00 +0530

    Added 152. Maximum Product Subarray problem to the randoms

---
+++ randoms/152. Maximum Product Subarray/maxProduct.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = nums[0];
+        int min = nums[0];
+        int res = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            int v1 = nums[i];
+            int v2 = nums[i] * max;
+            int v3 = nums[i] * min;
+
+            max = Math.max(v1, Math.max(v2, v3));
+            min = Math.min(v1, Math.min(v2, v3));
+
+            res = Math.max(res, Math.max(max, min));
+        }
+        return res;
+    }
+}
